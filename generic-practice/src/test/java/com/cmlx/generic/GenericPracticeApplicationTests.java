0b3acef41c2c1e3e085deaf197403e9a48be01f6
cmlx <zq861500116>
2020-12-26 15:12:23 +0800

    .feat:新增泛型

---
+++ generic-practice/src/test/java/com/cmlx/generic/GenericPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmlx.generic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
